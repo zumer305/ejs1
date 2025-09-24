16620eb712703cd4cf58daccf8f3bf6a4fec5e88
zumer305 <zumerniaz305@gmail.com>
2025-09-24 18:38:25 +0500

    push

--- index.js
+++ index.js
@@ -11,28 +11,21 @@ const express=require("express");
 const app=express();
 const port=8080;
 // npm install -g nodemon  (install)
-
-
 app.set("view engine","ejs");
 // app.set("views",path.join(__dirname,"/views")); //bahir sa server run ho to error na aya means jidr file prhi us sa peechy folder means cd .. wahan s run krna
-
-
-
-app.get("/",(req,res)=>{
-    // res.send("this is root"); array,html,strings
-    res.render("home.ejs");  //automatically express views folder ma sa home.ejs file dhondta
-
-})
-
-
-
-
-// --------------------------------------------------------------
 
 app.listen(port,()=>{
     console.log("listening");
 })
 
+
+// 1 
+// --------------------------------------------------------------
+app.get("/",(req,res)=>{
+    // res.send("this is root"); array,html,strings
+    res.render("home.ejs");  //automatically express views folder ma sa home.ejs file dhondta
+
+})
 // ----------------------------------------------------------------------
 
 
@@ -42,8 +35,26 @@ app.listen(port,()=>{
 // ejs.co             page 
 
 
+
+
+// 2
+// ----------------------------------------------------------------------
 app.get("/roler",(req,res)=>{
     let num=  Math.floor(Math.random()*6)+1;
 
     res.render("role.ejs",{num}); //num role.ejs ma bhja variable database syntax
 })
+// ----------------------------------------------------------------------
+
+
+
+
+
+// 3
+// ----------------------------------------------------------------------
+// Instagram activity
+app.get("/ig/:username",(req,res)=>{
+    let {username}= req.params;
+res.render("insta.ejs",{username});
+})
+// ----------------------------------------------------------------------
